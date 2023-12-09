05ec5be6be76ba117eda2795558ecf79e2f2d969
Edhotz <edmauroenterprise2021@gmail.com>
2023-12-09 07:23:25 +0100

    fix: refact navbar links

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,30 +1,5 @@
-"use client";
-
-import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  Center,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
-import Image from "next/image";
-
-import Link from "next/link";
-import { BsChevronLeft } from "react-icons/bs";
-import { API } from "@/services/api";
-import { useRouter } from "next/navigation";
-
 const About = () => {
-  return (
-    <Flex color="white" w="100%" h="100%">
-      About
-    </Flex>
-  );
+  return <div>About</div>;
 };
 
 export default About;
--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -1,29 +1,5 @@
-"use client";
-
-import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  Center,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
-import Image from "next/image";
-
-import Link from "next/link";
-import { BsChevronLeft } from "react-icons/bs";
-import { useRouter } from "next/navigation";
-
 const Analysis = () => {
-  return (
-    <Flex color="white" w="100%" h="100%">
-      Analysis
-    </Flex>
-  );
+  return <div>Analysis</div>;
 };
 
 export default Analysis;
